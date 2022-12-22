9de5c989d6f26bfcf03afd972ee8b69b2d43ea37
EngOmarSoudi <92035173+EngOmarSoudi@users.noreply.github.com>
2022-12-22 12:25:35 +0300

    some info

---
+++ section 4/main.js
@@ -0,0 +1,20 @@
+const vu = Vue.createApp( {
+    data ()
+    {
+        return {
+            message: "Hello World!",
+        };
+        
+    },
+    
+   
+   
+    
+} )
+vu.mount( "#app" );
+// setTimeout(() => {
+//     vu.mount( "#app" );
+// }, 3000 );
+// sertTimeout(() => {
+//     vu.unmount();
+// }, 6000 );
