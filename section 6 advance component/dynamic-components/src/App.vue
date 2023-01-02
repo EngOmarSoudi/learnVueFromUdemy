2acf015693b3deeacbe59788975382a8b08aa868
EngOmarSoudi <92035173+EngOmarSoudi@users.noreply.github.com>
2023-01-02 12:49:29 +0300

    Swich components

---
+++ section 6 advance component/dynamic-components/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+
+ <section>
+    <h1>Dynamic Components</h1>
+    <button @click="currentComponent = 'Home'">Home</button>
+    <button @click="currentComponent = 'About'">About</button>
+    <keep-alive>
+    <component :is="currentComponent"></component>
+    </keep-alive>
+  </section>
+  <select v-model="componentName">
+    <option value="Home">Home</option>
+    <option value="About">About</option>
+  </select>
+  <component :is="componentName"></component>
+</div> 
+</template>
+
+<script>
+import Home from './components/Home.vue';
+import About from './components/About.vue';
+export default {
+  name: "App"
+  ,components: {
+    Home,
+    About
+  },
+  data ()
+  {
+    return {
+      currentComponent: 'Home'
+      ,componentName: 'Home'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ section 6 advance component/dynamic-components/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Home Page</h1>
+</template>
+
+<script>
+export default {
+    unmounted ()
+    {
+        console.log("Home component Unmounted")
+    }
+}
+</script>
+
+<style>
+
+</style>
